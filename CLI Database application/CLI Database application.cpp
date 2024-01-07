41b4f1c86222cd215c1edf4fda2f70e2efafe9ad
Croudxd <benjamincroud@gmail.com>
2024-01-07 19:53:59 +0000

    Add project files.

---
+++ CLI Database application/CLI Database application.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+
+
+/**
+This is the backend of the CLI-Application i am building. The backend will use Crow library, Run on a raspberry pi 4.
+It will be connected to MySQL Database. Will have multiple features such as login, being able to modify the database using RESTapi.
+Other implementations and features as well, such as ToDoList, and more.
+**/
+
+int main()
+{
+
+}
+
