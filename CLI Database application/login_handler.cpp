0b9dee0940b07a0224746f267c17029477240877
Benjamin Croud <benjamincroud@gmail.com>
2024-01-11 18:16:17 +0000

    Added some comments.

--- CLI Database application/login_handler.cpp
+++ CLI Database application/login_handler.cpp
@@ -3,5 +3,22 @@
 
 crow::response login_handler::handleRequest(const crow::request& req)
 {
+	crow::json::wvalue response;
+	try {
+		auto jsonBody = crow::json::load(req.body);
+		
+		if (!jsonBody) 
+		{
+			throw::std::invalid_argument("Invalid JSON");
+		}
+
+		//
+		std::string username = jsonBody["username"].s();
+		std::string password = jsonBody["password"].s();
+		//access json and check use logic
+	}
+	catch (const std::exception& e) {
+
+	}
 
 }
